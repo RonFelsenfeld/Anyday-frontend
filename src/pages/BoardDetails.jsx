35d6579ef6ef8b6101540e1e7ae8f19eae56e1b0
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-04-04 23:11:59 +0300

    Clean and arrange code

--- src/pages/BoardDetails.jsx
+++ src/pages/BoardDetails.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router'
+
 import { boardService } from '../services/board.service'
+
 import { TaskList } from '../cmps/TaskList'
 import { BoardHeader } from '../cmps/BoardHeader'
+
 export function BoardDetails() {
   const [board, setBoard] = useState()
   const { boardId } = useParams()
   const navigate = useNavigate()
+
   useEffect(() => {
     if (boardId) loadBoard()
   }, [boardId])
+
   async function loadBoard() {
     try {
       const board = await boardService.getById(boardId)
@@ -19,12 +24,13 @@ export function BoardDetails() {
       navigate('/board')
     }
   }
+
   if (!board) return <div>Loading...</div>
   return (
     <section className="board-details">
       <BoardHeader />
-      {board.groups.map((group, idx) => (
-        <article key={group._id} className="board-group">
+      {board.groups.map(group => (
+        <article key={group.id} className="board-group">
           <h2 className="group-title">{group.title}</h2>
           <div className="group-content">
             <TaskList board={board} group={group} />
