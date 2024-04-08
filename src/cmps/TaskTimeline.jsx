e8369865ff317b6d2f482c3e40ecdc3a19b7fd6a
Atar Mor <atarmor92@gmail.com>
2024-04-09 01:59:59 +0300

    Add progress bar to timeline

--- src/cmps/TaskTimeline.jsx
+++ src/cmps/TaskTimeline.jsx
@@ -2,7 +2,7 @@ import { utilService } from '../services/util.service'
 
 export function TaskTimeline({ group, task }) {
   return (
-    <div className="task-row task-timeline" >
+    <div style={{background: `linear-gradient(to right, ${group.style.color} 67%, #333333 67%)`}} className="task-row task-timeline" >
       <div className="progress-bar">
         {task.timeline
           ? utilService.getFormattedTimeline(task.timeline.startDate, task.timeline.dueDate)
