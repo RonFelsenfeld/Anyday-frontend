603e085ad785de01286e1ac8451a84be119af96b
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-04-12 11:25:18 +0300

    Fix loader

--- src/cmps/Loader.jsx
+++ src/cmps/Loader.jsx
@@ -1,7 +1,7 @@
 export function Loader() {
   return (
     <div className="loader-container flex align-center justify-center">
-      <img src="/assets/img/loader.gif" alt="Loader gif" className="loader" />
+      <img src="/assets/img/loader.gif" alt="Loader gif" className="main-loader" />
     </div>
   )
 }
