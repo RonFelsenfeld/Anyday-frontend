71c2ea752895b695ae5150a5f112137bc62671f4
yotvat <idooy11@gmail.com>
2024-04-12 16:57:36 +0300

    Fix updateLog useEffect

--- src/cmps/UpdateLog.jsx
+++ src/cmps/UpdateLog.jsx
@@ -22,7 +22,7 @@ export function UpdateLog() {
 
   useEffect(() => {
     if (taskId) loadTask()
-  }, [])
+  }, [taskId])
 
   function loadTask() {
     const [taskGroup, task] = boardService.getTaskById(board, taskId)
