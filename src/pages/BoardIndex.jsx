29c662af9e07a2c8134a00c8fc79d0531e01eb3e
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-04-13 12:20:31 +0300

    Add board from board index - mobile view

--- src/pages/BoardIndex.jsx
+++ src/pages/BoardIndex.jsx
@@ -1,19 +1,36 @@
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+
 import { utilService } from '../services/util.service'
+import { loadBoards, saveBoard } from '../store/actions/board.actions'
+import { AddBoardBtn } from '../services/svg.service'
+import { boardService } from '../services/board.service'
 
 import { BoardList } from '../cmps/BoardList'
-import { loadBoards } from '../store/actions/board.actions'
 import { Loader } from '../cmps/Loader'
 
 export function BoardIndex() {
   const user = useSelector(storeState => storeState.userModule.loggedInUser)
   const isLoading = useSelector(storeState => storeState.systemModule.isLoading)
   const boards = useSelector(storeState => storeState.boardModule.boards)
+  const navigate = useNavigate()
 
   useEffect(() => {
     loadBoards()
   }, [boards.length])
+
+  // For mobile view
+  async function onAddBoard() {
+    try {
+      const board = boardService.getEmptyBoard()
+      const savedBoard = await saveBoard(board)
+      navigate(`/board/${savedBoard._id}`)
+      showSuccessMsg('We successfully added the board')
+    } catch (err) {
+      console.log('Could not add a board', err)
+    }
+  }
 
   let userGreeting = utilService.greetBasedOnHour() + ', '
   userGreeting += user ? `${user.fullName}!` : 'guest!'
@@ -35,6 +52,10 @@ export function BoardIndex() {
       </header>
 
       <BoardList boards={boards} />
+
+      <button className="btn btn-new-board mobile-view" onClick={onAddBoard}>
+        <AddBoardBtn />
+      </button>
     </section>
   )
 }
